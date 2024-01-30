84f23d2c54a14bc39c45da5aa934806e478a5f3d
gazi275 <gaziazad270@gmail.com>
2024-01-30 23:05:26 +0600

    first commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignUpForm from "../page/Register/SignUpForm";
+import Banner from "../Banner";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <SignUpForm></SignUpForm>
+    },
+    {
+        path: "banner",
+        element: <Banner></Banner>
     },
 ]);
 export default router;
